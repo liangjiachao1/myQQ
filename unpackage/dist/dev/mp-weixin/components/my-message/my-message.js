f015dd51bcebf7aa36d75b39c7e9ace6ed6f5cc8
梁嘉超 <2848511447@qq.com>
2023-12-28 19:14:06 +0800

    cs

--- unpackage/dist/dev/mp-weixin/components/my-message/my-message.js
+++ unpackage/dist/dev/mp-weixin/components/my-message/my-message.js
@@ -1,1 +1,24 @@
-Component({})
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["components/my-message/my-message"],{
+
+/***/ 63:
+/*!**************************************************************!*\
+  !*** E:/demo/myQQ/myQQ/components/my-message/my-message.vue ***!
+  \**************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'E:\\demo\\myQQ\\myQQ\\components\\my-message\\my-message.vue'");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/components/my-message/my-message.js.map
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/my-message/my-message-create-component',
+    {
+        'components/my-message/my-message-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('2')['createComponent'](__webpack_require__(63))
+        })
+    },
+    [['components/my-message/my-message-create-component']]
+]);
--- unpackage/dist/dev/mp-weixin/components/my-search/my-search.js
+++ unpackage/dist/dev/mp-weixin/components/my-search/my-search.js
@@ -1,1 +1,24 @@
-Component({})
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["components/my-search/my-search"],{
+
+/***/ 55:
+/*!************************************************************!*\
+  !*** E:/demo/myQQ/myQQ/components/my-search/my-search.vue ***!
+  \************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'E:\\demo\\myQQ\\myQQ\\components\\my-search\\my-search.vue'");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/components/my-search/my-search.js.map
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/my-search/my-search-create-component',
+    {
+        'components/my-search/my-search-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('2')['createComponent'](__webpack_require__(55))
+        })
+    },
+    [['components/my-search/my-search-create-component']]
+]);
